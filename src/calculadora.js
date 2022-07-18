f89bac2233b04a37897fcc912a9badf1cfa9eafa
Cassiano Elias <cassianoesssantana@gmail.com>
2022-07-18 13:22:19 -0300

    segundo commit

---
+++ src/calculadora.js
@@ -0,0 +1,21 @@
+//alert("qualquer")
+
+//let nome=document.getElementById("nome")
+
+//nome.value="Cassiano"
+function funcao(){
+    //alert("funcionou")
+    let largura1=documente.getElementById("largura1").value
+    let altura1=documente.getElementById("altura1").value
+    let area1=largura1+altura1
+let area1+ area2 
+    //alert("area total é " + area)
+    alert("area total é " + largura1 +" "+altura1)
+}
+document.getElementById("calcular").addEventListener("click", funcao);
+
+//document.getElementById("calcular).addEventListener("click", funcao);
+
+//Number.parseInt() - dentro do parenteses para transformar string em number - numero real
+
+//Number.parseFloat() - numeros quebrados com virgula
